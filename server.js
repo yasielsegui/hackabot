97ce58785bca7835cc4988823f2bfa6a611f4f79
yasielsegui <yasielsegui@live.com>
2016-06-12 15:19:59 -0400

    updated skype bot

--- server.js
+++ server.js
@@ -1,3 +1,40 @@
+const restify = require('restify');
+const skype = require('skype-sdk');
+const builder = require('botbuilder');
+
+// Initialize the BotService
+const botService = new skype.BotService({
+    messaging: {
+        botId: "dfbc5f3a-53a2-40fe-a123-abefca9f9a7a",
+        serverUrl : "https://apis.skype.com",
+        requestTimeout : 15000,
+        appId: process.env.BOTFRAMEWORK_APPID,
+        appSecret: process.env.BOTFRAMEWORK_APPSECRET
+    }
+});
+
+// Create bot and add dialogs
+var bot = new builder.SkypeBot(botService);
+bot.add('/', function (session) {
+   //respond with user's message
+    session.send("You said " + session.message.text);
+});
+
+// Setup Restify Server
+const server = restify.createServer();
+server.post('/api/messages', skype.messagingHandler(botService));
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+/*
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -22,11 +59,13 @@ var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // Serve a static web page
-server.get(/.*/, restify.serveStatic({
+server.get(/.*/  /*, restify.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
-
+*/
+/*
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+*/
