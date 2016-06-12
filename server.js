b5edd33becaeed763e8736d3836c95fa0f46f76d
yasielsegui <yasielsegui@live.com>
2016-06-12 15:32:08 -0400

    added debugger

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.get(/.*/, restify.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
-
+debugger;
 server.listen(process.env.port || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
