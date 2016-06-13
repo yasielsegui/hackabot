4a372735b479b138e210fff2502c980ecd654459
Segui <Yasiel.Segui@sungard.com>
2016-06-13 09:34:15 -0400

    commented serving index.html

--- server.js
+++ server.js
@@ -25,10 +25,11 @@ const server = restify.createServer();
 server.post('/api/messages', skype.messagingHandler(botService));
 
 // Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
+
+//server.get(/.*/, restify.serveStatic({
+//	'directory': '.',
+//	'default': 'index.html'
+//}));
 
 server.listen(process.env.port || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
