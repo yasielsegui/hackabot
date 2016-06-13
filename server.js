4baed49bdd1a2fe1e8f3a58134731fa392fd3bb3
Segui <Yasiel.Segui@sungard.com>
2016-06-13 16:51:07 -0400

    loggin the server object in json

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const botService = new skype.BotService({
     }
 });
 
-
-
-
-
 // Create bot and add dialogs
 console.log("Creating SkypeBot ...");
 
@@ -25,14 +21,12 @@ botService.on('contactAdded', (bot, data) => {
     console.log("Helloooooo");
     bot.reply('Hello', true);
 });
-
 botService.on('personalMessage', (bot, data) => {
     console.log("Helloooooo");
     
     bot.reply('Helloooooo', true);
 });
 */
-
 
 var bot = new builder.SkypeBot(botService);
 bot.add('/', function (session) {
@@ -42,14 +36,12 @@ bot.add('/', function (session) {
     session.send("HELLOoooooooooooooo");
 });
 
-
 // Setup Restify Server
 console.log("Creating restify server ...");
 const server = restify.createServer();
 server.post('/api/messages', skype.messagingHandler(botService));
 
 // Serve a static web page
-
 //console.log("Configuring the restify server to serve the bot home page ...");
 //server.get(/.*/, restify.serveStatic({
 //	'directory': '.',
@@ -58,41 +50,7 @@ server.post('/api/messages', skype.messagingHandler(botService));
 
 
 server.listen(process.env.port || 8080, function () {
-   console.log('%s listening to %s', server.name,  "X->" + server.url); 
+   console.log('%s listening to %s', server.name, server.url); 
+   console.log(JSON.stringify(server));
 });
 
-/*
-var restify = require('restify');
-var builder = require('botbuilder');
-
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
-};
-
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-
-// Serve a static web page
-server.get(/.*/  /*, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-*/
-/*
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
-*/
